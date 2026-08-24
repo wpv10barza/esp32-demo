d6939a067fcbfb2613bb4d4800ba6e7cd819f37a
wpv10barza <polovaleriow@gmail.com>
2026-08-23 21:38:33 -0500

    Add local secrets template

---
+++ secrets.example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Copy this file as secrets.h and edit ONLY the values below.
+// secrets.h is ignored by Git and must never be committed.
+
+#define WIFI_SSID_VALUE "YOUR_WIFI_NAME"
+#define WIFI_PASSWORD_VALUE "YOUR_WIFI_PASSWORD"
+#define CALENDAR_URL_VALUE "YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL"
